666c9191516e82a1b65e2a10205a87411118f634
Everton <imevertonrch@gmail.com>
2023-03-28 10:46:16 -0300

    Primeiro componente react, Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+
+function Banner() {
+  // JSX
+  return (
+    <header className="banner">
+      <img src="images/banner.png" alt="Banner image" />
+    </header>
+  );
+}
+
+export default Banner;
