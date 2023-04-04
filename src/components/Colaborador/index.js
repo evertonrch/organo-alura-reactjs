e22b664ae958b2798d03685a4d32c8ce62cc07c8
Everton <imevertonrch@gmail.com>
2023-04-04 14:58:04 -0300

    Importando ícone do botão deletar

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,11 +1,15 @@
 import "./Colaborador.css";
+import { AiFillCloseSquare } from "react-icons/ai";
 
 const Colaborador = function ({ nome, img, cargo, corFundo, aoDeletar }) {
   return (
     <div className="colaborador">
-      <div className="deletar" onClick={aoDeletar}>
-        deletar
-      </div>
+      <AiFillCloseSquare
+        className="deletar"
+        size={30}
+        cursor={"pointer"}
+        onClick={aoDeletar}
+      />
       <div className="cabecalho" style={{ backgroundColor: corFundo }}>
         <img src={img} alt={nome}></img>
       </div>
